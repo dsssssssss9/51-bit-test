e45d08b6913d02c6763b1ab84dca187d4cd4ff6a
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-26 21:32:44 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    colorbit_51bit.setMultiColor(true)
+    colorbit_51bit.showColorIcon(ColorIcon.Ghost, colorbit.colors(BitColors.Black))
+    colorbit_51bit.setMultiColor(false)
+})
+let colorbit_51bit: colorbit.Strip = null
+colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
+colorbit_51bit.clear()
+colorbit_51bit.showScrollStringColor("Hello!", colorbit.colors(BitColors.Red))
+colorbit_51bit.setBrightness(128)
+basic.forever(function () {
+	
+})
