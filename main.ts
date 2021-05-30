ee457a85ce20373ca8bf169b285e1262e909ca38
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-30 17:14:58 +0100

    changes for video 1

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
-input.onButtonPressed(Button.A, function () {
+let colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
+colorbit_51bit.clear()
+colorbit_51bit.showScrollStringColor("Hello There ...I am a ColorBit", colorbit.colors(BitColors.Red))
+colorbit_51bit.setBrightness(32)
+colorbit_51bit.clear()
+colorbit_51bit.setMultiColor(true)
+colorbit_51bit.showStringColor("ColorBit", colorbit.colors(BitColors.Green))
+colorbit_51bit.setMultiColor(false)
+basic.forever(function () {
     colorbit_51bit.setMultiColor(true)
     colorbit_51bit.showColorIcon(ColorIcon.Ghost, colorbit.colors(BitColors.Black))
     colorbit_51bit.setMultiColor(false)
 })
-let colorbit_51bit: colorbit.Strip = null
-colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
-colorbit_51bit.clear()
-colorbit_51bit.showScrollStringColor("Hello!", colorbit.colors(BitColors.Red))
-colorbit_51bit.setBrightness(32)
